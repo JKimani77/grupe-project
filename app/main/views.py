b7e36900dcc9f4a0061fea173c9d7d072618a4b8
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-05 00:29:06 +0300

    new post view

--- app/main/views.py
+++ app/main/views.py
@@ -10,4 +10,19 @@ def index():
     posts = Post.query.all()
     
     return render_template('index.html', posts= posts)
-    
+
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post_info = form.post_info.data
+        image = form.image.data
+        user_id = current_user
+        new_post_object = Post(post_info=post_info,user_id=current_user._get_current_object().id,image=image,title=title)
+        new_post_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('new_post.html', form = form)
+
