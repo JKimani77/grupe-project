9c87ca2ac9b22a8511ddee2ee473bba97ea66090
JKimani77 <38456207+JKimani77@users.noreply.github.com>
2024-03-21 13:49:38 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,13 +1,13 @@
-$env:CLIENT_SECRET='nleq82u342^*@Y*u*DHE@'
-$env:CLIENT_ID = 'b22c15292965b5d03d7a'
-$env:ACCESS_TOKEN = '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'
-$env:API_KEY = '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'
+$env:CLIENT_SECRET=''
+$env:CLIENT_ID = ''
+$env:ACCESS_TOKEN = ''
+$env:API_KEY = ''
 $env:REPO_URL = 'https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}'
 $env:USER_URL = 'https://api.github.com/search/users?q={query}{&page}'
-$env:BASE_URL = 'https://api.github.com/JKKimani77/grupe-project?access_token=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'
+$env:BASE_URL = 'https://api.github.com/JKKimani77/grupe-project?access_token='
 
-$env:MAIL_USERNAME='jkkimani77@gmail.com'
-$env:MAIL_PASSWORD='Ivonne1236987'
+$env:MAIL_USERNAME=''
+$env:MAIL_PASSWORD=''
 
 
 python manage.py server
